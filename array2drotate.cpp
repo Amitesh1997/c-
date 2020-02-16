b544393676f34a22e56432dd995bb9b37e92d3b0
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2020-02-17 03:27:36 +0530

    as

---
+++ array2drotate.cpp
@@ -0,0 +1,40 @@
+//RAY COMPETATIVE CODING TEMPLATE
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll MOD = 1e+7;
+int main(){
+	
+	int n;
+	cin >> n;
+	int a[n][n];
+	
+	for(int i = 0; i<n; i++)
+		for(int j = 0; j<n; j++)
+			cin >> a[i][j];
+		
+		int aa=0, bb=n-1,temp;
+		for(int i = 0; i<n; i++)
+			{
+				for(int j = 0; j<n; j++){
+				    temp=a[j][bb];
+                 	a[j][bb] = a[aa][j];
+			    a[aa][j]=temp;
+					
+				aa++, bb--; 
+			}
+		}
+			
+			cout << "\nAfter rotating the array :\n";
+			
+			for(int i = 0; i<n; i++)
+			{
+				for(int j = 0; j<n; j++)
+					cout << a[i][j] << " ";
+					
+					cout << endl;
+			}
+			
+	
+	return 0;	
+}
